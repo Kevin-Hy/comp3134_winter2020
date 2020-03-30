317fa5ab2962fd3edf03671fffdcee9a0fad4e3e
Kevin <kevin.hy@georgebrown.ca>
2020-03-30 16:13:54 -0400

    Lab 8 Partial

---
+++ Lab8/cfsr.php
@@ -0,0 +1,7 @@
+<?php
+<form method="POST">
+        <label for="username">Username:</label> <input type="username" id="usename" name="username"><br>
+        <label for="password">Password:</label> <input type="password" id="password" name="password">
+        <button type="submit">Login</button>
+</form>
+?>
